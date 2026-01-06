b0553ea4de789327fcfedc3bb67510f90fdfbe9b
juanmr8 <moraromerojuan8@gmail.com>
2026-01-06 20:17:15 +0100

    initial progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import ParagraphGradient from '@/components/paragraph';
+
+const text = "It is long established that the reader will be distracted by the readable content of a page when looking at its layout."
 export default function Page() {
-  return null;
+  return (
+	<div>
+		<ParagraphGradient content={text} />
+	</div>
+  );
 }
---
+++ components/paragraph.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ParagraphGradient({ content }: { content: string }) {
+	return <div>{content}</div>;
+}
+
+export default ParagraphGradient;
